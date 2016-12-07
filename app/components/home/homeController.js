5c76645d1faa60200edab73451e2b161da02e108
Ethan Weeks <ethan.weeks@techfino.com>
2016-12-06 18:28:40 -0700

    updated carousel by removing side controls

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -7,7 +7,7 @@ function home($scope){
 	vm.title = "Welcome to Marketplace!";
 	vm.subtitle = "A basic marketplace application built with Angular v1.";
 
-	$scope.slides = [
+	$scope.links = [
 		{src: './assets/img/download.jpeg',  caption: 'Picture 1'},    
 		{src: './assets/img/download1.jpeg', caption: 'Picture 2'},    
 		{src: './assets/img/download2.jpeg', caption: 'Picture 3'}
--- app/shared/carousel/carouselDirective.js
+++ app/shared/carousel/carouselDirective.js
@@ -1,17 +1,16 @@
-angular.module('carousel', []).directive('carousel', ['$timeout', carousel]);
-
-function carousel($timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      slides: '='
-    },
-    templateUrl: './app/shared/carousel/carouselView.html',
-    link: function(scope, element) {
-      $timeout(function() {
-        $('.carousel-indicators li',element).first().addClass('active');
-        $('.carousel-inner .item',element).first().addClass('active');
-      });
-    }
-  };
-}
+angular.module('carousel', [])
+  .directive('carousel', ['$timeout', function($timeout) {
+    return {
+      restrict: 'E',
+      scope: {
+        links: '='
+      },
+      templateUrl: './app/shared/carousel/carouselView.html',
+      link: function(scope, element) {
+        $timeout(function() {
+          $('.carousel-indicators li',element).first().addClass('active');
+          $('.carousel-inner .item',element).first().addClass('active');
+        });
+      }
+    };
+  }]);
